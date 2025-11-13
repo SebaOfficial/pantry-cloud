a801f3f93c235a1cf7d20d2894dd9943a3c64ed2
Sebastiano Racca <sebastiano@racca.me>
2025-11-13 14:50:05 +0100

    fix: handling client exceptions

--- src/Exceptions/RequestException.php
+++ src/Exceptions/RequestException.php
@@ -2,6 +2,20 @@
 
 namespace Pantry\Exceptions;
 
+use Throwable;
+
 class RequestException extends PantryException
 {
+    private int $httpCode;
+
+    public function __construct(string $message, int $httpCode, int $code = 0, ?Throwable $previous = null)
+    {
+        $this->httpCode = $httpCode;
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getHttpCode(): int
+    {
+        return $this->httpCode;
+    }
 }
